e9ec27f93ec65857ff0f7642ebb723432780b198
aquacalc <aquacalc@yahoo.com>
2025-02-11 23:00:39 -0500

    refactor company details API endpoint by uncommenting code and removing unnecessary comments

--- src/routes/company-details/+server.js
+++ src/routes/company-details/+server.js
@@ -1,23 +1,23 @@
-// import { json } from '@sveltejs/kit';
-
-// export async function GET({ fetch }) {
-// 	const response = await fetch('/src/lib/server/my-data.json');
-
-// 	const companyDetails = await response.json();
-
-// 	return json(companyDetails);
-// }
-
 import { json } from '@sveltejs/kit';
 
 export async function GET({ fetch }) {
-    const response = await fetch('/src/lib/server/my-data.json');
+	const response = await fetch('/src/lib/server/my-data.json');
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch company details');
-    }
+	const companyDetails = await response.json();
 
-    const companyDetails = await response.json();
-
-    return json(companyDetails);
+	return json(companyDetails);
 }
+
+// import { json } from '@sveltejs/kit';
+
+// export async function GET({ fetch }) {
+//     const response = await fetch('/src/lib/server/my-data.json');
+
+//     if (!response.ok) {
+//         throw new Error('Failed to fetch company details');
+//     }
+
+//     const companyDetails = await response.json();
+
+//     return json(companyDetails);
+// }
