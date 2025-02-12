dfce972c14ddcd15ffd79f7c6cfaf471e09322b5
aquacalc <aquacalc@yahoo.com>
2025-02-12 10:29:49 -0500

    update SvelteKit configuration to use Vercel adapter and add CORS support for company details API

--- src/routes/company-details/+server.js
+++ src/routes/company-details/+server.js
@@ -5,7 +5,27 @@ export async function GET({ fetch }) {
 
 	const companyDetails = await response.json();
 
-	return json(companyDetails);
+	if (!response.ok) {
+		throw new Error('Failed to fetch company details');
+	}
+
+	return json(companyDetails, {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+		}
+	});
+}
+
+export async function OPTIONS() {
+	return new Response(null, {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+		}
+	});
 }
 
 // import { json } from '@sveltejs/kit';
