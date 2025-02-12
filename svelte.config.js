c6758e50469477516efdf10c8a687cd8433ce568
aquacalc <aquacalc@yahoo.com>
2025-02-11 22:38:56 -0500

    add CSRF configuration to SvelteKit adapter

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		// see: https://stackoverflow.com/questions/78628151/how-to-publish-a-forward-facing-api-in-sveltekit
+		csrf: {
+			checkOrigin: false //<--- Add this line
+		}
 	}
 };
 
